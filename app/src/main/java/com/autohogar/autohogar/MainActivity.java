e53c65678b78cc75348d5ec3d7fae065e50f65ca
pablord <pablo.rd14@gmail.com>
2016-12-13 21:05:07 -0300

    Orientacion de activity solo vertical.

--- app/src/main/java/com/autohogar/autohogar/MainActivity.java
+++ app/src/main/java/com/autohogar/autohogar/MainActivity.java
@@ -1,5 +1,6 @@
 package com.autohogar.autohogar;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
@@ -11,17 +12,17 @@ import android.webkit.WebViewClient;
 public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
-    private String paginaWeb = "http://www.google.com/";
+    private String paginaWeb = "http://192.168.0.107:8000/web/";
+    //private String paginaWeb = "http://www.google.com/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         webView = (WebView) this.findViewById(R.id.activity_main_webview);
-        //WebSettings webSettings = webView.getSettings();
-        //webSettings.setJavaScriptEnabled(true);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
         webView.loadUrl(paginaWeb);
         //webView.setWebViewClient(new WebViewClient());
         //webView.setWebViewClient(new MyAppWebViewClient());
