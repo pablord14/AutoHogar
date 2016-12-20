57dbf43ebe3463a71c8efe3246a9572d17430454
pablord <pablo.rd14@gmail.com>
2016-12-20 19:29:36 -0300

    Navegabilidad solucionada.

--- app/src/main/java/com/autohogar/autohogar/MainActivity.java
+++ app/src/main/java/com/autohogar/autohogar/MainActivity.java
@@ -1,32 +1,44 @@
 package com.autohogar.autohogar;
 
 import android.content.pm.ActivityInfo;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.Formatter;
 import android.view.Window;
 import android.view.WindowManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
     private String paginaWeb = "http://192.168.0.107:8000/web/";
-    //private String paginaWeb = "http://www.google.com/";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        //Conexion c = new Conexion();
+        //c.start();
+
         webView = (WebView) this.findViewById(R.id.activity_main_webview);
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        webView.loadUrl(paginaWeb);
-        //webView.setWebViewClient(new WebViewClient());
-        //webView.setWebViewClient(new MyAppWebViewClient());
 
+        WebView myWebView = (WebView) findViewById(R.id.activity_main_webview);
+        myWebView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl(getPaginaWeb());
 
     }
 
@@ -37,4 +49,9 @@ public class MainActivity extends AppCompatActivity {
             super.onBackPressed();
         }
     }
+
+
+    public String getPaginaWeb() {return paginaWeb;}
+
+    public void setPaginaWeb(String paginaWeb) {this.paginaWeb = paginaWeb;}
 }
